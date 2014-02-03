25dee34ea4f6609e0501ed54e8dc6cc3e46c5646
Adam W <adamnfmitchell@users.noreply.github.com>
2014-02-03 20:42:09 +1100

    Change database technology to PDO
    You really, really need to refactor this code. Right now, in your other PHP files there is a massive SQL injection vulnerability. Parameterized PDO will assist you to avoid this.

--- connect.php
+++ connect.php
@@ -7,6 +7,13 @@ $dbpass							= "";
 //$dbuser							= "lexpioneer";
 //$dbpass							= "lex@pioneers";
 $dbname							= "magnum_hiprojects";
-$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die ("Error connecting to database");
-mysql_select_db($dbname);
+
+try {
+  $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass); 
+}
+catch (PDOException $e) {
+  // Handle the database connection error
+} 
+
+
 ?>
